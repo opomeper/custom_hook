d4ff36ab57b727befbd9c46de78b158747edbe55
Opomeper <saurojoemer14@gmail.com>
2025-05-14 18:55:47 +0800

    Updated UseFetch to FetchData and created FetchYoga

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYoga'
 
 function App() {
   return (
  <>
- <FetchData/>
+ {/* <FetchData/> */}
+ <FetchYogaData/>
  </>
   )
 }
